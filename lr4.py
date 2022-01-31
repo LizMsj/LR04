ef4198e5af4a7a62ed0f33d024e060969d357c27
Мойсей Елизавета <Moysey9g@mail.ru>
2022-01-31 18:41:44 +0300

    Create lr4.py

---
+++ lr4.py
@@ -0,0 +1,17 @@
+def pt():
+    x = input('Введите путь к папке: ')
+    dictionary(x)
+  
+def dictionary(x):
+    pass
+
+def duble():
+    pass
+
+def fin():
+    pass
+  
+if __name__ == '__main__':
+    d1 = dict() # словарь - ключ (путь до файла): значение(размер файла)
+    pt()
+    
