dcbfe047ff86cfe1c58def77fb58325ca07f148d
Мойсей Елизавета <Moysey9g@mail.ru>
2022-02-01 08:53:29 +0300

    Update lr4.py

--- lr4.py
+++ lr4.py
@@ -1,3 +1,5 @@
+from os import *
+
 def pt():
     x = input('Введите путь к папке: ')
     if path.isdir(x) == True:
@@ -8,7 +10,7 @@ def pt():
 def dictionary(x):
     for i in listdir(x):
         if path.isdir(x + "\\" + i):
-            dictionary(x+ '\\' + i, level+1)
+            dictionary(x+ '\\' + i)
         elif path.isfile(x + '\\' + i):
             name = x + "\\" + i
             size = stat(x + "\\" + i).st_size
@@ -16,13 +18,47 @@ def dictionary(x):
             A.append(i)
     return d1, A
 
-def duble():
-    pass
+def duble(d1, A):
+    size = list(d1.values())
+    pop = list(d1.keys())
+    D = [i for i, x in enumerate(size) if size.count(x) > 1]
+    D1 = [i for i, x in enumerate(A) if A.count(x) > 1]
+    index = []
+    d1.clear()
+    help_in_way = []
+    if len(D) >= len(D1):
+        for x in D:
+            if x in D1:
+                index.append(x)
+    else:
+        for x in D1:
+            if x in D:
+                index.append(x)
+    for x in index:
+        t = [A[x], size[x]]
+        d1[pop[x]]= t
+        help_in_way.append(A[x])
+    fin(d1)
 
-def fin():
-    pass
+def fin(d1):
+    print()
+    if len(d1)==0:
+        print("Дубликатов нет")
+    else:
+        help_in_way = list()
+        for x in d1.values():
+            if x not in help_in_way:
+                help_in_way.append(x)
+        help_in_way.sort()
+        for y in help_in_way:
+            print("Дубликаты ", y)
+            p = [k for k in d1 if d1[k] == y]
+            for i in p:
+                print(str(i).replace("\\\\", "\\"))
+            print()
   
 if __name__ == '__main__':
     d1 = dict() # словарь - ключ (путь до файла): значение(размер файла)
     pt()
+    duble(d1, A)
     
